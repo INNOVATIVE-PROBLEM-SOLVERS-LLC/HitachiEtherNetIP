2e856ee61d70e5e6e7bbb1422c1bd450d0b01916
MarvinThompson <marv@visi.com>
2019-04-19 13:09:47 -0500

    Fix deleting QR33 font.

--- Hitachi_Browser_Test/H_EIP/TestEIP.cs
+++ Hitachi_Browser_Test/H_EIP/TestEIP.cs
@@ -16,10 +16,13 @@ namespace H_EIP {
          // Comment out next line if browser not needed
          browser = new Browser("192.168.0.1", 44818, @"C:\Temp\EIP");
          if (browser == null) {
+            // Get a new EtherNet/IP instance
             EIP = new EIP("192.168.0.1", 44818, @"C:\Temp\EIP");
          } else {
+            // Use the instance from the browser
             EIP = browser.EIP;
          }
+         // Sample of using pre-defined dropdowns
          cbFont.Items.AddRange(EIP.DropDowns[(int)fmtDD.FontType]);
       }
 
@@ -55,24 +58,25 @@ namespace H_EIP {
                int cols = EIP.GetAttribute(ccPF.Number_Of_Columns); // Get the number of columns
                EIP.SetAttribute(ccIDX.Automatic_reflection, 1);     // Stack up all the operations
                if (cols > 1) { // No need to delete columns if there is only one
-                  EIP.SetAttribute(ccIDX.Column, 1); // Select column 2 (0 origin on deletes)
+                  EIP.SetAttribute(ccIDX.Column, 1);                // Select column 2 (0 origin on deletes)
                   for (int i = 2; i <= cols; i++) {
-                     EIP.ServiceAttribute(ccPF.Delete_Column); // Keep deleting column 2
+                     EIP.ServiceAttribute(ccPF.Delete_Column);      // Keep deleting column 2
                   }
                }
-               EIP.SetAttribute(ccIDX.Item, 1);           // Set column 1(1 origin on Line Count)
-               EIP.SetAttribute(ccPF.Line_Count, 1);      // Set to 1 line
-               EIP.SetAttribute(ccPF.Dot_Matrix, "5x8");  // Set the format to something small
-               for (int i = 2; i <= 5; i++) {             // Add four more columns
+               EIP.SetAttribute(ccIDX.Item, 1);                 // Set column 1(1 origin on Line Count)
+               EIP.SetAttribute(ccPF.Line_Count, 1);            // Set to 1 line
+               EIP.SetAttribute(ccPF.Barcode_Type, "not used"); // Just in case it is a QR33
+               EIP.SetAttribute(ccPF.Dot_Matrix, "5x8");        // Set the format to something small
+               for (int i = 2; i <= 5; i++) {                   // Add four more columns
                   EIP.ServiceAttribute(ccPF.Add_Column);
                }
-               EIP.SetAttribute(ccIDX.Item, 2);      // Stack column 2
+               EIP.SetAttribute(ccIDX.Item, 2);                 // Stack column 2
                EIP.SetAttribute(ccPF.Line_Count, 2);
-               EIP.SetAttribute(ccIDX.Item, 4);      // Stack column 4
+               EIP.SetAttribute(ccIDX.Item, 4);                 // Stack column 4
                EIP.SetAttribute(ccPF.Line_Count, 2);
                for (int i = 1; i <= 7; i++) {
-                  EIP.SetAttribute(ccIDX.Item, i);   // Select item
-                  if (i == 1 || i == 4 || i == 7) {  // Set the font and text
+                  EIP.SetAttribute(ccIDX.Item, i);              // Select item
+                  if (i == 1 || i == 4 || i == 7) {             // Set the font and text
                      EIP.SetAttribute(ccPF.Print_Character_String, $"{i}");
                      EIP.SetAttribute(ccPF.Dot_Matrix, "12x16");
                   } else {
